db28b4914ad94dba6e56db4274af8dc34a4648ea
kmd178 <kmd178@gmail.com>
2017-03-27 16:32:24 +0300

    Step2 Interrupts
    Step1 reads the values of the switches and buttons of the Zedboard and
    prints them out to the screen using the UART peripheral. The program
    lab2.c iterates and actively samples the two GPIO peripherals to read in
    their values. This technique, called polling, is not very efficient
    because it requires that the CPU spends all if its cycles waiting for
    external events to take place. In Step2  the Generic Interrupt
    Controller (GIC) of the Zynq7000 PS subsystem is used which manages
    interrupts that are coming from different PL and PS sources to the CPU.
    It is a centralized resource which is capable of enabling, disabling,
    masking and prioritizing interrupt sources sending them to the CPU(s) in
    a programmed manner. Once the CPU receives an interrupt from the
    interrupt controller, it will save the state of the executing process
    and will use the interrupt ID to determine which interrupt handler (aka
    interrupt service routine, ISR) to invoke to service the interrupt.

---
+++ 3.InterruptsSDK/Cortex_A9_wrapper_hw_platform_0/ps7_init.c
@@ -0,0 +1,135 @@
+#include "xparameters.h"
+#include "xgpio.h"
+#include "xscugic.h"
+#include "xil_exception.h"
+#include "xil_printf.h"
+
+// Parameter definitions
+#define INTC_DEVICE_ID 		   XPAR_PS7_SCUGIC_0_DEVICE_ID
+#define BTNS_DEVICE_ID		   XPAR_GPIO_0_DEVICE_ID
+#define INTC_GPIO_INTERRUPT_ID XPAR_FABRIC_BTNS_5BIT_IP2INTC_IRPT_INTR
+
+#define BTN_INT 			   XGPIO_IR_CH1_MASK
+
+XGpio BTNInst;
+XScuGic INTCInst;  // Generic Interrupt Controller
+static int btn_value;
+static int counter_value = 0;
+
+//----------------------------------------------------
+// PROTOTYPE FUNCTIONS
+//----------------------------------------------------
+static void BTN_Intr_Handler(void *baseaddr_p);
+static int InterruptSystemSetup(XScuGic *XScuGicInstancePtr);
+static int IntcInitFunction(u16 DeviceId, XGpio *GpioInstancePtr);
+
+//----------------------------------------------------
+// INTERRUPT HANDLER FUNCTIONS
+// - called by the timer, button interrupt, performs
+// - LED flashing
+//----------------------------------------------------
+
+
+void BTN_Intr_Handler(void *InstancePtr)
+{
+	// Disable GPIO interrupts
+	XGpio_InterruptDisable(&BTNInst, BTN_INT);
+	// Ignore additional button presses
+	if ((XGpio_InterruptGetStatus(&BTNInst) & BTN_INT) !=
+			BTN_INT) {
+			return;
+		}
+	btn_value = XGpio_DiscreteRead(&BTNInst, 1);
+
+	if (btn_value!=1)
+	{
+		counter_value++;
+	}
+	else{
+		counter_value=0;
+	}
+	xil_printf("Counter= %x\r\n", counter_value);
+
+
+    (void)XGpio_InterruptClear(&BTNInst, BTN_INT);
+
+    // Enable GPIO interrupts
+    XGpio_InterruptEnable(&BTNInst, BTN_INT);
+}
+
+//----------------------------------------------------
+// MAIN FUNCTION
+//----------------------------------------------------
+int main (void)
+{
+  int status;
+  //----------------------------------------------------
+  // INITIALIZE THE PERIPHERALS & SET DIRECTIONS OF GPIO
+  //----------------------------------------------------
+  // Initialise Push Buttons
+  status = XGpio_Initialize(&BTNInst, BTNS_DEVICE_ID);
+  if(status != XST_SUCCESS) return XST_FAILURE;
+  // Set all buttons direction to inputs
+  XGpio_SetDataDirection(&BTNInst, 1, 0xFF);
+
+  // Initialize interrupt controller
+  status = IntcInitFunction(INTC_DEVICE_ID, &BTNInst);
+  if(status != XST_SUCCESS) return XST_FAILURE;
+
+
+  while(1); // endless loop
+
+  return 0;
+}
+
+//----------------------------------------------------
+// INITIAL SETUP FUNCTIONS
+//----------------------------------------------------
+
+int InterruptSystemSetup(XScuGic *XScuGicInstancePtr)
+{
+	// Enable interrupt
+	XGpio_InterruptEnable(&BTNInst, BTN_INT);
+	XGpio_InterruptGlobalEnable(&BTNInst);
+
+	Xil_ExceptionRegisterHandler(XIL_EXCEPTION_ID_INT,
+			 	 	 	 	 	 (Xil_ExceptionHandler)XScuGic_InterruptHandler,
+			 	 	 	 	 	 XScuGicInstancePtr);
+	Xil_ExceptionEnable();
+
+
+	return XST_SUCCESS;
+
+}
+
+int IntcInitFunction(u16 DeviceId, XGpio *GpioInstancePtr)
+{
+	XScuGic_Config *IntcConfig;
+	int status;
+
+	// Interrupt controller initialisation
+	IntcConfig = XScuGic_LookupConfig(DeviceId);
+	status = XScuGic_CfgInitialize(&INTCInst, IntcConfig, IntcConfig->CpuBaseAddress);
+	if(status != XST_SUCCESS) return XST_FAILURE;
+
+	// Call to interrupt setup
+	status = InterruptSystemSetup(&INTCInst);
+	if(status != XST_SUCCESS) return XST_FAILURE;
+
+	// Connect GPIO interrupt to handler
+	status = XScuGic_Connect(&INTCInst,
+					  	  	 INTC_GPIO_INTERRUPT_ID,
+					  	  	 (Xil_ExceptionHandler)BTN_Intr_Handler,
+					  	  	 (void *)GpioInstancePtr);
+	if(status != XST_SUCCESS) return XST_FAILURE;
+
+	// Enable GPIO interrupts interrupt
+	XGpio_InterruptEnable(GpioInstancePtr, 1);
+	XGpio_InterruptGlobalEnable(GpioInstancePtr);
+
+	// Enable GPIO and timer interrupts in the controller
+	XScuGic_Enable(&INTCInst, INTC_GPIO_INTERRUPT_ID);
+
+	return XST_SUCCESS;
+}
+
